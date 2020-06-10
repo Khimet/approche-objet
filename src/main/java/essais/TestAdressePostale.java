7972554ae0ecfb09cb2822b15abe074338faced0
Khalil HIMET <khalil.himet@outlook.com>
2020-06-10 14:50:45 +0200

    TP 2

--- src/main/java/essais/TestAdressePostale.java
+++ src/main/java/essais/TestAdressePostale.java
@@ -5,8 +5,8 @@ import entites.AdressePostale;
 public class TestAdressePostale {
 
 	public static void main(String[] args) {
-		AdressePostale a1 = new AdressePostale();
-		AdressePostale a2 = new AdressePostale();
+		AdressePostale a1 = new AdressePostale(22, "boulevard jean moulin", 14200, "Nantes");
+		AdressePostale a2 = new AdressePostale(1, "rue de la paix", 75000, "Paris");
 
 	}
 
--- src/main/java/essais/TestPersonne.java
+++ src/main/java/essais/TestPersonne.java
@@ -7,18 +7,21 @@ import entites.Personne;
 public class TestPersonne {
 
 	public static void main(String[] args) {
-		Personne a = new Personne();
+		// Utilisation du deuxième constructeur
+		Personne a = new Personne("Dupont", "Marc", new AdressePostale(4, "BD", 50000, "Nice"));
 		a.nom = "Dupont";
 		a.prenom = "Yann";
-		a.adresse = new AdressePostale();
-		a.adresse.codePostal = 14000;
+//		a.adresse = new AdressePostale(1, "RUE", 17, "Paris");
+//		a.adresse.codePostal = 14000;
 		a.adresse.numeroRue = 189;
 		
-		Personne b = new Personne();
+		Personne b = new Personne("Bob", "Mob");
 		b.nom = "Monsieur";
 		b.prenom = "Jean";
-		b.adresse = new AdressePostale();
+		b.adresse = new AdressePostale(4, "Allée", 68, "Tourcoing");
 		b.adresse.ville = "Paris";
+		
+		System.out.println(a.adresse.codePostal);
 			
 		
 
