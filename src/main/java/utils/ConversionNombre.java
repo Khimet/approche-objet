51bd5d4832e333bf1373e98799479d853a7b9a0b
Khalil HIMET <khalil.himet@outlook.com>
2020-06-10 17:27:11 +0200

    TP 4

---
+++ src/main/java/utils/ConversionNombre.java
@@ -0,0 +1,18 @@
+package utils;
+
+public class ConversionNombre {
+	public static void main(String[] args) {
+		String nb = "1";
+		
+		int result = Integer.parseInt(nb);
+		
+		int a = 4;
+		int b = 12;
+		
+		int max = Integer.max(a, b);
+		
+		System.out.println(max);
+		
+	}
+
+}
