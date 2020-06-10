42b2cc7d9b12ed086b6ab2059052165555a92039
Khalil HIMET <khalil.himet@outlook.com>
2020-06-10 12:06:35 +0200

    TP 1

---
+++ src/main/java/essais/TestPersonne.java
@@ -0,0 +1,23 @@
+package essais;
+
+import entites.Personne;
+
+
+public class TestPersonne {
+
+	public static void main(String[] args) {
+		Personne a = new Personne();
+		a.nom = "Dupont";
+		a.prenom = "Yann";
+		a.adressePostale = "Paris";
+		
+		Personne b = new Personne();
+		b.nom = "Monsieur";
+		b.prenom = "Jean";
+		b.adressePostale = "Toulouse";
+			
+		
+
+	}
+
+}
---
+++ src/test/java/org/kh/approche_objet_kh/AppTest.java
@@ -0,0 +1,20 @@
+package org.kh.approche_objet_kh;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
