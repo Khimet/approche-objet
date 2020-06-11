fccc8e5345653d78cb6c2438a69ba99df71b3450
Khalil HIMET <khalil.himet@outlook.com>
2020-06-11 11:27:07 +0200

    TP 5

---
+++ src/main/java/fr/diginamic/banque/TestBanque.java
@@ -0,0 +1,14 @@
+package fr.diginamic.banque;
+
+import fr.diginamic.banque.entites.Compte;
+
+public class TestBanque {
+
+	public static void main(String[] args) {
+		Compte a = new Compte("AC1478956", 156);
+		
+		System.out.println(a);
+
+	}
+
+}
---
+++ src/main/java/fr/diginamic/banque/entites/Compte.java
@@ -0,0 +1,16 @@
+package fr.diginamic.banque.entites;
+
+public class Compte {
+	
+	private String numeroCompte;
+	private double soldeCompte;
+	
+	public Compte(String numeroCompte, double soldeCompte) {
+		super();
+		this.numeroCompte = numeroCompte;
+		this.soldeCompte = soldeCompte;
+	}
+	
+	
+
+}
